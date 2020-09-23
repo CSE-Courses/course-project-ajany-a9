7c73ff903edbf8741e9178f1e87423ee9629d7fa
yaodongl <36283361+yaodongl@users.noreply.github.com>
2020-09-22 23:16:02 -0400

    Add files via upload
    I built a general framework on android studio. Three main buttons have been added, class schedule, class, and professor. Each button implements the listener, you can add your own function in the corresponding area.

---
+++ app/src/main/java/com/example/beststudy/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.beststudy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private Button Schedule;
+    private Button Class;
+    private Button Professor;
+    private TextView ToBeAdded;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        //initial button with id which set on activity_main.xml
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        this.Schedule=(Button)this.findViewById(R.id.schedule);
+        this.Class=(Button)this.findViewById(R.id.Class);
+        this.Professor=(Button)this.findViewById(R.id.Professor);
+
+        //the Schedule method gonna implement to the function below
+        this.Schedule.setOnClickListener(new View.OnClickListener(){
+
+            public void onClick(View v) {
+
+            }
+        });
+
+        //the Class method gonna implement to the function below
+        this.Class.setOnClickListener(new View.OnClickListener(){
+
+            public void onClick(View v) {
+
+            }
+        });
+
+        //the Professor method gonna implement to the function below
+        this.Professor.setOnClickListener(new View.OnClickListener(){
+
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
