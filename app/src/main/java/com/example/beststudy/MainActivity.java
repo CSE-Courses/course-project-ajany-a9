d06b13298c36d966b8333f7774a3b6764d3e9bf7
adamjbarah <36278076+adamjbarah@users.noreply.github.com>
2020-09-24 20:42:11 -0400

    Created class grade calculator
    Created the class grade calculator and a new button on the home page which is used to access it

--- app/src/main/java/com/example/beststudy/MainActivity.java
+++ app/src/main/java/com/example/beststudy/MainActivity.java
@@ -2,26 +2,29 @@ package com.example.beststudy;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     private Button Schedule;
     private Button Class;
     private Button Professor;
+    private Button Grades;
     private TextView ToBeAdded;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         //initial button with id which set on activity_main.xml
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         this.Schedule=(Button)this.findViewById(R.id.schedule);
         this.Class=(Button)this.findViewById(R.id.Class);
         this.Professor=(Button)this.findViewById(R.id.Professor);
+        this.Grades=(Button)this.findViewById(R.id.Grades);
 
         //the Schedule method gonna implement to the function below
         this.Schedule.setOnClickListener(new View.OnClickListener(){
@@ -44,6 +47,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
             public void onClick(View v) {
 
+            }
+        });
+
+        //Clicking "Grades" button opens grade calculator screen
+        this.Grades.setOnClickListener(new View.OnClickListener(){
+
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, GradeCalc.class);
+                startActivity(intent);
             }
         });
     }
