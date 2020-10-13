fe8637b891a62fceb80371510e6742f59d7ea067
Jessi <jessiwu@buffalo.edu>
2020-10-12 21:30:23 -0400

    Task #28 - Used SQLite and created insert and remove methods to store and update the list of assignments

--- app/src/main/java/com/example/beststudy/AssignmentScreen.java
+++ app/src/main/java/com/example/beststudy/AssignmentScreen.java
@@ -1,5 +1,6 @@
 package com.example.beststudy;
 
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
@@ -16,10 +17,14 @@ import java.util.Random;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class AssignmentScreen extends AppCompatActivity {
+
+    Database db;
+
+    private Button button;
+    private EditText input;
     private ArrayList<String> assignments;
     private ArrayAdapter<String> arrayAdapter;
     private ListView listView;
-    private Button button;
 
 
     @Override
@@ -27,28 +32,20 @@ public class AssignmentScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tasks);
 
-        listView = findViewById(R.id.listView);
+        db = new Database(this);
+
+        assignments = new ArrayList<>();
         button = findViewById(R.id.button);
+        input = findViewById(R.id.editText2);
+        listView = findViewById(R.id.listView);
 
-        button.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view){
-                addAssignment(view);
-            }
-        });
+        viewData();
 
-        assignments = new ArrayList<String>();
-        arrayAdapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,assignments);
-        listView.setAdapter(arrayAdapter);
-        listViewListener();
-    }
-
-    private void listViewListener(){
         listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
 
             private final Random random = new Random();
 
-            //USER STORY #4 Random Encouragement Message
+            //User Story #4 Random Encouragement Message
             @Override
             public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
                 String[] toastMessages = new String[]{"Good Job!", "Awesome!", "Well Done!", "Nice!",
@@ -58,24 +55,40 @@ public class AssignmentScreen extends AppCompatActivity {
                 toast.setGravity(Gravity.CENTER_VERTICAL,0,0);
                 toast.show();
 
+                db.removeData(assignments.get(i));
                 assignments.remove(i);
                 arrayAdapter.notifyDataSetChanged();
                 return true;
             }
         });
+
+        button.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                String description = input.getText().toString();
+                if(!description.equals("") && db.insertData(description)){
+                    Toast.makeText(AssignmentScreen.this,"Assignment Added", Toast.LENGTH_SHORT).show();
+                    input.setText("");
+                    assignments.clear();
+                    viewData();
+                }else{
+                    Toast.makeText(AssignmentScreen.this,"Assignment Not Added", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
-    private void addAssignment(View view){
-        EditText input = findViewById(R.id.editText2);
-        String description = input.getText().toString();
+    private void viewData(){
+        Cursor cursor = db.viewData();
 
-        if(!(description.equals(""))){
-            arrayAdapter.add(description);
-            input.setText("");
-        }
-        else{
-            Toast.makeText(getApplicationContext(),"Please add in a valid description",Toast.LENGTH_LONG).show();
+        if(cursor.getCount() == 0){
+            Toast.makeText(this, "No assignment to show", Toast.LENGTH_SHORT).show();
+        }else{
+            while(cursor.moveToNext()){
+                assignments.add(cursor.getString(1));
+            }
+            arrayAdapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1, assignments);
+            listView.setAdapter(arrayAdapter);
         }
     }
-
 }
--- app/src/main/java/com/example/beststudy/Database.java
+++ app/src/main/java/com/example/beststudy/Database.java
@@ -1,4 +1,62 @@
 package com.example.beststudy;
 
-public class Database {
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class Database extends SQLiteOpenHelper {
+
+    private static final String DB_NAME = "Assignments.db";
+    private static final String DB_TABLE = "Assignments_Table";
+
+    //Columns
+    private static final String NAME = "NAME";
+
+    private static final String CREATE_TABLE = "CREATE TABLE "+ DB_TABLE+" ("+ " INTEGER PRIMARY KEY AUTOINCREMENT, "+ NAME+ " TEXT"+ ")";
+
+
+    public Database(Context context){
+        super(context, DB_NAME, null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS "+ DB_TABLE);
+        onCreate(db);
+    }
+
+    //create method to insert data
+    public boolean insertData(String name){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValue = new ContentValues();
+        contentValue.put(NAME, name);
+
+        long result = db.insert(DB_TABLE, null, contentValue);
+
+        return result != -1;
+    }
+
+    //create method to remove data
+    public boolean removeData(String name){
+        SQLiteDatabase db = this.getWritableDatabase();
+        long result = db.delete(DB_TABLE, "NAME=?", new String[]{name});
+
+        return result > 0;
+    }
+
+    //create method to view data
+    public Cursor viewData(){
+        SQLiteDatabase db = this.getReadableDatabase();
+        String query = "Select * from "+DB_TABLE;
+        Cursor cursor = db.rawQuery(query, null);
+
+        return cursor;
+    }
 }
